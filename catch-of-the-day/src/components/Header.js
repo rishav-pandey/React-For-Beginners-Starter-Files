181967cf381b86fe3a159e65c62bcbb9eddfa42e
rishav-pandey <27219302+rishav-pandey@users.noreply.github.com>
2018-10-17 00:22:20 +0530

    adding application layout with components

---
+++ catch-of-the-day/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = props => (
+  <header className="top">
+    <h1>Catch
+      <span className="ofThe">
+        <span className="of">of</span>
+        <span className="the">the</span>
+      </span>
+      the Day
+    </h1>
+    <h3 className="tagline">
+      <span>Fresh Seafood Market</span>
+    </h3>
+  </header>
+);
+
+export default Header;
