16fa23d9ea6ad1b784d502198797d5bf21d2b62e
rishav-pandey <27219302+rishav-pandey@users.noreply.github.com>
2018-10-17 20:59:34 +0530

    event handling

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { getFunName } from '../helpers'
 
-class StorePicker extends React.Component {
+class StorePicker extends Component {
+  myInput = React.createRef();
+  // constructor() {
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+  goToStore = (event) => {
+    // 1. Stop the form from submitting
+    event.preventDefault();
+    // 2. get the text from the input
+    const storeName = this.myInput.value.value;
+    // 3. Change the page to /store/:storeId
+    this.props.history.push(`/store/${storeName}`)
+  }
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+        <input
+          type="text"
+          required
+          ref={this.myInput}
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit Store -></button>
       </form>
     )
